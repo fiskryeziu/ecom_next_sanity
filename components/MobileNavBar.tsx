a1b05cb5166087ec4f0f842d7de0f0d9fa3c0db9
fiskryeziu <fiskryeziu@gmail.com>
2023-02-06 17:28:27 +0100

    adding open/close navmenu

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -7,12 +7,19 @@ interface IState {
 }
 const MobileNavBar = ({ setOpen, open }: IState) => {
   return (
-    <div className="fixed w-screen h-full  z-10">
+    <div
+      className={`fixed w-screen h-full top-0 ${
+        open ? ' left-[0px]' : 'left-[-3000px]'
+      } z-10 transition-all ease-in-out duration-700`}
+    >
       <div className="flex absolute left-0 top-0 w-72 h-full bg-[#f7f7f7] z-10">
         <div className="w-full relative flex flex-col">
-          <div className="p-2 border border-black absolute right-5 top-6">
-            <AiOutlineClose size={18} onClick={() => setOpen(!open)} />
-          </div>
+          <button
+            className="p-2 border border-black absolute right-5 top-6"
+            onClick={() => setOpen(!open)}
+          >
+            <AiOutlineClose size={18} />
+          </button>
           <div className="mt-24">
             <p className="px-4 py-2 font-semibold">Home</p>
             <p className="px-4 py-2 font-semibold">Home</p>
