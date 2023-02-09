9c65410a8400475af911869f1cb32352d2d240b8
fiskryeziu <fiskryeziu@gmail.com>
2023-02-09 14:47:42 +0100

    adding card component

---
+++ components/Card.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React, { useState } from 'react'
+import product from '../public/assets/watchproduct.jpg'
+import { FaShoppingBasket } from 'react-icons/fa'
+
+const Card = () => {
+  const [show, setShow] = useState(false)
+  return (
+    <div className="relative flex w-full md:w-1/4 max-h-80 overflow-hidden items-center justify-center border group">
+      <div className="flex flex-col gap-1 w-56">
+        <div className="w-full h-56">
+          <Image
+            src={product}
+            alt="trending-watch"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <p className="text-slate-400 text-xs">Watch</p>
+        <p className="text-sm font-medium">Nice Leather Watch</p>
+        <p className="text-right font-semibold">$200</p>
+        <div className="w-full z-40 cursor-pointer flex  gap-2 justify-center invisible -translate-y-10 transition-transform ease  duration-300 group-hover:visible group-hover:translate-y-0">
+          <FaShoppingBasket size={16} className="text-slate-400" />
+          <p className="text-slate-400 text-sm">Add to cart</p>
+        </div>
+        <div className="absolute w-full h-full top-0 left-0 group-hover:bg-white opacity-50 group-hover:duration-300 duration-300"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Banner from '@/components/Banner'
+import Card from '@/components/Card'
 import Watches from '@/components/Watches'
 import { IProduct } from '@/typings'
 import Image from 'next/image'
@@ -10,7 +11,15 @@ export default function Home({ product }: { product: IProduct[] }) {
     <>
       <Banner />
       <Watches />
-      {/* <Card/> */}
+      <div className="flex flex-col">
+        <p className="text-2xl font-semibold text-center my-10">Trending Now</p>
+        <div className="flex flex-col md:flex-row px-2 sm:px-10 lg:px-32 gap-5 lg:gap-10">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
       {/* <TopBrands/> */}
       {/* <InfoSection/> */}
       {/* <States/> */}
